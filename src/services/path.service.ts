e4ac986ca9f37c454cd20d41f3799c796cf5de47
aketza <aketza.eguskiza@kaytek.io>
2025-07-11 08:34:13 +0200

    Update path.service.ts

--- src/services/path.service.ts
+++ src/services/path.service.ts
@@ -1,25 +1,25 @@
 import { getConfiguration } from "../lib/configuration.js";
 
 export default class PathService {
-	static getWebUrl(appendix: string) {
-		return `${getConfiguration("base")["web_url"]}/${appendix}`.toLocaleLowerCase();
-	}
+  static getWebUrl(appendix: string) {
+    return `${getConfiguration("base")["web_url"]}/${appendix}`.toLocaleLowerCase();
+  }
 
-	static getRoute(appendix: string): string {
-		return this.getWebUrl(`/#/${appendix}`);
-	}
+  static getRoute(appendix: string): string {
+    return this.getWebUrl(`#/${appendix}`);
+  }
 
-	static getWikiViewRoute(appendix: string): string {
-		return `${getConfiguration("views")["wiki"]}/${appendix}`.toLocaleLowerCase();
-	}
+  static getWikiViewRoute(appendix: string): string {
+    return `${getConfiguration("views")["wiki"]}/${appendix}`.toLocaleLowerCase();
+  }
 
-	static getPascalCase(name: string): string {
-		return name.substring(0, 1).toUpperCase().concat(name.substring(1));
-	}
+  static getPascalCase(name: string): string {
+    return name.substring(0, 1).toUpperCase().concat(name.substring(1));
+  }
 
-	static getFullWikiPath(subPath: string, appendix: string): string {
-		return this.getWebUrl(
-			`${getConfiguration("path")["wiki"]}/${subPath.substring(0, subPath.lastIndexOf("/"))}/${appendix}`,
-		);
-	}
+  static getFullWikiPath(subPath: string, appendix: string): string {
+    return this.getWebUrl(
+      `${getConfiguration("path")["wiki"]}/${subPath.substring(0, subPath.lastIndexOf("/"))}/${appendix}`,
+    );
+  }
 }
