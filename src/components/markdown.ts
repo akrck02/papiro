2055aa435d7c4f58c9b84c8502f19161b9c76ef3
akrck02 <aketza.vazquez@gmail.com>
2025-07-27 11:56:23 +0200

    adding lazy loading for images

--- src/components/markdown.ts
+++ src/components/markdown.ts
@@ -1,4 +1,4 @@
-import { setDomEvents, uiComponent } from "../lib/dom.js";
+import { setDomAttributes, setDomEvents, uiComponent } from "../lib/dom.js";
 import { Html } from "../lib/html.js";
 import { getIcon } from "../lib/icons.js";
 import { IconBundle, MaterialIcons } from "../model/enum/icons.js";
@@ -6,53 +6,85 @@ import PathService from "../services/path.service.js";
 import WikiService from "../services/wiki.service.js";
 
 export default class MarkdownCanvas {
-  static readonly CLASS = "markdown";
-  static create(markdown: string): HTMLElement {
-    const markdownCanvas = uiComponent({
-      classes: [MarkdownCanvas.CLASS],
-      text: WikiService.render(markdown),
-    });
+	static readonly CLASS = "markdown";
+	static create(markdown: string): HTMLElement {
+		const canvas = uiComponent({
+			classes: [MarkdownCanvas.CLASS],
+			text: WikiService.render(markdown),
+		});
 
-    markdownCanvas.querySelectorAll("img").forEach((img) => {
-      const imgHtml = img as HTMLImageElement;
-      const webUrl = PathService.getWebUrl();
-      if (-1 != imgHtml.src.indexOf(webUrl)) {
-        const newUrl = img.src.substring(PathService.getWebUrl().length);
-        imgHtml.src = PathService.getFullWikiResourcePath(newUrl);
-      }
-    });
+		this.prepareImageTags(canvas);
 
-    markdownCanvas.querySelectorAll("a").forEach((img) => {
-      const aHtml = img as HTMLAnchorElement;
-      const webUrl = PathService.getWebUrl();
-      if (-1 != aHtml.href.indexOf(webUrl)) {
-        const newUrl = img.href.substring(PathService.getWebUrl().length);
-        aHtml.href = PathService.getWikiViewRoute(newUrl);
-      }
-    });
+		canvas.querySelectorAll("a").forEach((img) => {
+			const aHtml = img as HTMLAnchorElement;
+			const webUrl = PathService.getWebUrl();
+			if (-1 != aHtml.href.indexOf(webUrl)) {
+				const newUrl = img.href.substring(PathService.getWebUrl().length);
+				aHtml.href = PathService.getWikiViewRoute(newUrl);
+			}
+		});
 
-    markdownCanvas.querySelectorAll("pre code").forEach((code) => {
-      const codeHtml = code as HTMLElement;
-      const copyButton = uiComponent({
-        type: Html.Button,
-        classes: ["copy-button"],
-        text: getIcon(
-          IconBundle.Material,
-          MaterialIcons.ContentCopy,
-          "1rem",
-          "var(--surface-6)",
-        ).outerHTML,
-      });
+		canvas.querySelectorAll("pre code").forEach((code) => {
+			const codeHtml = code as HTMLElement;
+			const copyButton = uiComponent({
+				type: Html.Button,
+				classes: ["copy-button"],
+				text: getIcon(
+					IconBundle.Material,
+					MaterialIcons.ContentCopy,
+					"1rem",
+					"var(--surface-6)",
+				).outerHTML,
+			});
 
-      setDomEvents(copyButton, {
-        click: () => {
-          navigator.clipboard.writeText(codeHtml.innerText);
-        },
-      });
+			setDomEvents(copyButton, {
+				click: () => {
+					navigator.clipboard.writeText(codeHtml.innerText);
+				},
+			});
 
-      codeHtml.appendChild(copyButton);
-    });
+			codeHtml.appendChild(copyButton);
+		});
 
-    return markdownCanvas;
-  }
+		return canvas;
+	}
+
+	private static prepareImageTags(canvas: HTMLElement) {
+		const options = {
+			root: canvas,
+			rootMargin: "0px",
+			scrollMargin: "0px",
+			threshold: 1.0,
+		};
+
+		const imageObserver = new IntersectionObserver((entries, observer) => {
+			entries.forEach((entry) => {
+				if (!entry.isIntersecting) return;
+
+				const img = entry.target as HTMLImageElement;
+				img.src = img.dataset.src;
+				img.onload = () => {
+					img.classList.remove("lazy");
+					observer.unobserve(img);
+				};
+			});
+		}, options);
+
+		canvas.querySelectorAll("img").forEach((img) => {
+			const imgHtml = img as HTMLImageElement;
+			imgHtml.loading = "lazy";
+			imgHtml.classList.add("lazy");
+
+			const webUrl = PathService.getWebUrl();
+			if (-1 != imgHtml.src.indexOf(webUrl)) {
+				const newUrl = img.src.substring(PathService.getWebUrl().length);
+				imgHtml.src = PathService.getFullWikiResourcePath(newUrl);
+			}
+
+			img.dataset.src = img.src;
+			img.src = undefined;
+
+			imageObserver.observe(img);
+		});
+	}
 }
